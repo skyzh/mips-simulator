2df9952c1080dc7c580ba5aa937559a2c0f5d1f6
Alex Chi <iskyzh@gmail.com>
2020-04-02 00:01:17 +0800

    add load instruction memory test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,9 +5,19 @@ import           Memory
 import           Utils
 import           Prelude                 hiding ( take )
 import           Data.Vector
+import           Data.Word
 
 main :: IO ()
 main = do
-  mem <- loadIMem "test/naive-tests/1-test-add.hex"
+    runTestTT tests
+    return ()
+
+testLoadIMem = TestCase $ do
+  mem <- loadIMem "test/naive-tests/0-imem.hex"
   let Memory content = mem
-  putStrLn $ show (take 32 content)
+  assertEqual "0 byte" (content ! 0) 0xde
+  assertEqual "1 byte" (content ! 1) 0xad
+  assertEqual "2 byte" (content ! 2) 0xbe
+  assertEqual "3 byte" (content ! 3) 0xef
+
+tests = TestList [TestLabel "test load instruction memory" testLoadIMem]
